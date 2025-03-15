fa1c1f1e85ab427a1e54574976b088bafef6c144
Craig Robinson <craig@teamrobinson.us>
2025-03-14 21:59:52 -0400

    add okd specific values

--- 00_set-variables.sh
+++ 00_set-variables.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Specify Cluster Name, Prism Central password, and OpenShift Version
-export CLUSTER_NAME=            # Ex: homelab1 homelab2
-export PC_PASSWORD=             # Located 
-export VERSION=4.14.10   #Version List - https://mirror.openshift.com/pub/openshift-v4/clients/ocp/
+export CLUSTER_NAME=okdlab            # Ex: homelab1 homelab2
+export PC_PASSWORD=Nutanix/4u            # Located 
+export VERSION=4.17.0   #Version List - https://mirror.openshift.com/pub/openshift-v4/clients/ocp/
 
 # Common variables
-export PRISM_CENTRAL_FQDN=prism-central.mydc.dev
+export PRISM_CENTRAL_FQDN=prism-central.lab.mydc.dev
 export INSTALL_DIR=./ocp-install
 export RELEASE_ARCH=amd64
 export BASE_DOMAIN=mydc.dev
@@ -17,15 +17,15 @@ export CLUSTER_NETWORK=10.128.0.0/14
 export SERVICE_NETWORK=172.30.0.0/16
 
 # cluster specific variables
-if [ $CLUSTER_NAME = 'homelab1' ]; then
-    export NTNX_PE_SUBNET_UUID=251ddf3f-2eb8-4947-b330-5d60911d4305
-    export NTNX_PE_UUID=00060e9d-43b4-5243-791b-246e963cb590
+if [ $CLUSTER_NAME = 'okdlab' ]; then
+    export NTNX_PE_SUBNET_UUID=34004d18-7119-4a4d-8f2b-410b30141a17
+    export NTNX_PE_UUID=00063002-8083-7b9c-50ea-3448ede7ae8b
     export MACHINE_NETWORK=192.168.10.0/23
-    export API_VIP=192.168.10.21
-    export INGRESS_VIP=192.168.10.22
-    export PE_ENDPOINT=192.168.10.17
-    export DATA_SERVICE_ENDPOINT=
-    export STORAGE_CONTAINER=homelab1-volumes
+    export API_VIP=192.168.10.11
+    export INGRESS_VIP=192.168.10.12
+    export PE_ENDPOINT=192.168.10.25
+    export DATA_SERVICE_ENDPOINT=192.168.10.26
+    export STORAGE_CONTAINER=okdlab
 else
     :
 fi
--- 01_fetch-binaries.sh
+++ 01_fetch-binaries.sh
@@ -5,10 +5,9 @@ source 00_set-variables.sh
 mkdir -p /tmp/ocp-tools
 cd /tmp/ocp-tools
 
-curl -Ls -o ccoctl-linux.tar.gz https://mirror.openshift.com/pub/openshift-v4/$RELEASE_ARCH/clients/ocp/$VERSION/ccoctl-linux.tar.gz
-curl -Ls -o openshift-client-linux.tar.gz https://mirror.openshift.com/pub/openshift-v4/$RELEASE_ARCH/clients/ocp/$VERSION/openshift-client-linux.tar.gz
-curl -Ls -o openshift-install-linux.tar.gz https://mirror.openshift.com/pub/openshift-v4/$RELEASE_ARCH/clients/ocp/$VERSION/openshift-install-linux.tar.gz
-
+curl -Ls -o ccoctl-linux.tar.gz https://github.com/okd-project/okd/releases/download/4.17.0-okd-scos.0/ccoctl-linux-4.17.0-okd-scos.0.tar.gz
+curl -Ls -o openshift-client-linux.tar.gz https://github.com/okd-project/okd/releases/download/4.17.0-okd-scos.0/openshift-client-linux-4.17.0-okd-scos.0.tar.gz
+curl -Ls -o openshift-install-linux.tar.gz https://github.com/okd-project/okd/releases/download/4.17.0-okd-scos.0/openshift-install-linux-4.17.0-okd-scos.0.tar.gz
 tar xzvf ccoctl-linux.tar.gz
 tar xzvf openshift-client-linux.tar.gz
 tar xzvf openshift-install-linux.tar.gz
